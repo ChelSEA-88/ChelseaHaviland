ae83885ec8656b26d725c152c739fe378baac0d5
Chelsea Haviland <chelsea.haviland.13@gmail.com>
2020-11-15 16:35:10 -0500

    successfully rendered headshot

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 // import Hero from "../components/Hero";
+
+//Components
 import Container from "../Components/Container";
 import Row from "../Components/Row";
 import Col from "../Components/Col";
+import Headshot from "../Pictures/chelseagit.png";
 
 function About() {
     return (
       <div>
-        <Container style={{ marginTop: 30 }}>
+        <Container style={{ marginTop: 80 }}>
           <Row>
             <Col size="md-12">
               <h1>Hey there!</h1>
@@ -15,20 +18,17 @@ function About() {
           </Row>
           <Row>
             <Col size="md-6">
-            <p>I am a creative at heart. I love bringing things together, wether it's
-              an event, a space, toppings on a pizza, or a layout on a page. Although
-              this new tech adventure is a different kind of challenge than I have ever
-              faced, I have thoroughly enjoyed every BOOM moment that comes after 
-              countless hours of failed attempts and head scratching moments. </p>
-            <p>I look for the beauty in everything, and everyone. I love  my children, even
-              though they ask for too many snacks. I love anything my husband puts on the
-              smoker. I love my French Bulldog and my American Foxhound. Ten months out 
-              of the year I wish it were October, the other month I get to wear green. 
-              I can't decide which I like better: hiking to a sweeping mountain view or 
-              reading on the beach listening to oceanwaves. 
-              
-              </p>
+                <p>
+                    I am a creative at heart. I love bringing things together, wether it's an event, a space, toppings on a pizza or a layout on a page. Althought this new tech adventure is a different kind of challenge than I have ever faced, I have thoroughly enjoyed every BOOM  moment that comes after countless hours of failed attempts and bang your head on the table moments. 
+                </p>
+                <p>
+                    I look for the beauty in everything, and everyone. I love my children, even though they ask for too many snacks. I love anything my husband puts on the smoker. I love my French Bulldog and my American Foxhound. Ten months out of the year I wish it were October, and the other month I get to wear green. I can't decide which I like better: hiking to a sweeping mountain view or reading on the beach while listening to oceanwaves.
+                </p>
             </Col>
+            <Col size="6">
+                <img src={Headshot} alt="Headshot"/>
+            </Col>
+
           </Row>
         </Container>
       </div>
